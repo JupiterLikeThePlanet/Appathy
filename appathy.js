5c7e7b5a1bcdd16961ade4a34c822b95f1bd1759
JupiterLikeThePlanet <jupiterbaudot@gmail.com>
2015-11-07 13:56:00 -0800

    onClick of SoWhat adds text to text field. Jake

--- appathy.js
+++ appathy.js
@@ -3,13 +3,19 @@ $(document).ready(function() {
 
   // $('p').css('color','red')
 
-  // $('.md > textarea')
-
-  var our_button = $('<input type="button" value="so what"/>');
+  asciiArt = "TESTING";
+  var our_button = $("<button id='so_what' type='button'>so what</button>");
   // ^ name a js variable (our_button) and set it equal to inserting an element. (our element type is a button and the name of the button (value) is "so what"). we are inserting an element using jquery syntax
 
   $('.usertext-buttons').append(our_button);
   // ^ appends (aka adds) our "so what" button next to the save button
   // did not append to $('.save') because that put our_button inside the save button when what we wanted was to put our_button inside the container "usertext-buttons" in order to put our_button next to the save button
+
+  // $('.md > textarea').val(asciiArt)
+   //do this onclick
+
+  $('#so_what').on('click',function(){
+    $('.md textarea').val(asciiArt);
+  })
 
 });
